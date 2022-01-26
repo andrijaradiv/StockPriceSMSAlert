19fbfea6e96ac6ed9cd20ec4d691e3ddf193286b
andrijaradiv <andrija567123@gmail.com>
2022-01-26 14:07:10 -0500

    config file

---
+++ config.py
@@ -0,0 +1,10 @@
+ACCOUNT_SID = 'Your Twilio Account SID' # Account SID from Twilio
+AUTH_TOKEN = 'Your Twilio Auth Token' # Auth token from twilio.com/console
+YAHOO_API_KEY = 'Your Yahoo api key' # Change this to your Yahoo API key
+
+PHONE_NUMBER = 'Your Phone numver' # Change this to your phone number
+FROM_NUMBER = 'Twilio phone number' # Change this to your Twilio number
+
+TICKERS = ['AAPL', 'TESLA', 'BTC', 'ETH'] # Change this to your ticker symbols
+
+PERCENTAGE_CHANGE_THRESHOLD = 10 # Change this to your daily % threshold
